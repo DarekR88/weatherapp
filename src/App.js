e986d5cdeade82fb3b362b622eb0af5d9f3eb800
DarekR88 <dradmclad@gmail.com>
2019-08-19 21:29:26 -0500

    chart.js working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Chart from './components/resultsGraph';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Chart city='Houston' />
     </div>
   );
 }
--- src/components/resultsGraph.js
+++ src/components/resultsGraph.js
@@ -1,22 +1,63 @@
-import React, {Component} from 'react';
-import {Line} from 'react-chartjs-2';
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import API from '../utils/API';
 
-class Chart extends Component{
-    state = {
-        chartData: {
-            labels: [],
-            datasets:[
-                {
-                    label: '',
-                    data: []
-                }
-            ]
+class Chart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            results: [],
+            chartData: {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'Temperature',
+                        data: []
+                    }
+                ]
+            }
         }
     }
-    render(){
-        return(
+
+
+
+    componentDidMount() {
+        this.searchWeather(this.props.city)
+
+    }
+
+    searchWeather = query => {
+        API.search(query)
+            .then(res => this.createChartData(res.data.list))
+            .catch(err => console.log(err));
+    }
+
+    createChartData = (results) => {
+        let temps = [];
+        let dates = [];
+        for (let i = 0; i < results.length; i++) {
+                temps.push(results[i].main.temp_max)
+                dates.push(results[i].dt_txt)
+        }
+        console.log(temps)
+        console.log(dates)
+        this.setState({
+            chartData: {
+                labels: dates,
+                datasets: [
+                    {
+                        label: 'Temperature',
+                        data: temps
+                    }
+                ]
+            }
+        })
+    }
+
+    render() {
+        return (
             <div className='chart'>
-                <Line 
+                <Line
                     data={this.state.chartData}
                     options={{}}
                 />
